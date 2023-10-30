e423652e9d22a84113898dd58b91efa87d9b1600
abro3569 <abigail.robinson@natgeo.su.se>
2023-10-30 09:39:38 +0100

    [edit] ndwi + dyn world code

---
+++ ndwi_area_process.py
@@ -0,0 +1,67 @@
+import os
+import rasterio.features
+from shapely.geometry import shape, MultiPolygon
+import geopandas as gpd
+import csv
+
+wetland_name = 'Tarnsjon'
+
+# Folder containing the input raster files
+input_folder = r"Z:\ndwi\area\ndwi_int\ndwi_mask_" + wetland_name
+
+print(input_folder)
+
+# Specify the CRS for reprojecting to UTM (replace 'EPSG:32633' with the appropriate UTM zone)
+utm_crs = "EPSG:32633"
+
+# Create an empty list to store the results
+results = []
+
+# Iterate over files in the folder
+for filename in os.listdir(input_folder):
+    if filename.endswith(".tif"):  # Assuming the raster files have the .tif extension
+        raster_path = os.path.join(input_folder, filename)
+
+        # Use a context manager to open the raster file
+        with rasterio.open(raster_path) as src:
+            # Read the first band (index 0) instead of band 1
+            binary_array = src.read(1)
+
+            # Make sure you read the transform inside the context manager
+            transform = src.transform
+
+        # Get shapes (polygons)
+        shapes = list(rasterio.features.shapes(binary_array, transform=transform))
+
+        # Extract polygons using a list comprehension
+        polygons = [shape(geom) for geom, value in shapes if value == 1]  # Filter by the desired value (e.g., 1)
+
+        # Dissolve the polygons into a single polygon and buffer it by 0 (no buffer)
+        dissolved_polygon = MultiPolygon(polygons).buffer(0)
+
+        # Create a GeoDataFrame with the dissolved polygon
+        dissolved_gdf = gpd.GeoDataFrame(geometry=[dissolved_polygon])
+
+        # Set the CRS of the GeoDataFrame to WGS84 (EPSG:4326)
+        dissolved_gdf.crs = "EPSG:4326"
+
+        # Reproject the GeoDataFrame to UTM (or your desired projected CRS)
+        dissolved_gdf = dissolved_gdf.to_crs(utm_crs)
+
+        # Calculate the area in square meters (m²) in the projected CRS
+        area_utm = dissolved_gdf.geometry.area.iloc[0]
+
+        # Optionally, you can print the area for each raster
+        print(f"{filename} Area (UTM): {area_utm} m²")
+
+        # Append the results to the list
+        results.append((filename, area_utm))
+
+
+
+output_csv = r'C:\Users\abro3569\PycharmProjects\gee\ndwi_area\\' + wetland_name + '.csv'
+with open(output_csv, mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Filename", "Area (metres squared)"])
+    for filename, area in results:
+        writer.writerow([filename, area])
--- ndwi_area_stats.py
+++ ndwi_area_stats.py
@@ -3,29 +3,29 @@ import os
 import calendar
 import matplotlib.pyplot as plt
 
-wetland_names = ['Aloppkolen']
+wetland_name = 'Tarnsjon'
 
 # Locate csv file and load as a dataframe
-file_path = r'Z:\ndwi\area\Aloppkolen.csv'
+file_path = r'C:\Users\abro3569\PycharmProjects\gee\ndwi_area\\' + wetland_name + '.csv'
+
 df = pd.read_csv(file_path, delimiter=',', header=0)
 
-# Extract year and month
-df['year'] = df['Polygon Name'].str.extract(r'year_(\d+)').astype(int)
-df['month'] = df['Polygon Name'].str.extract(r'month_(\d+)').astype(int)
+# Extract year and month correctly
+df[['year', 'month']] = df['Filename'].str.extract(r'_(\d{4})_(\d+)\.tif')
 
 # Create a new column with the desired format
-df['Date'] = df['month'].apply(lambda x: f'{x:02d}') + '/' + df['year'].astype(str)
+df['Date'] = df['month'].str.zfill(2) + '/' + df['year']
 
-# Give df a new name
-df['Name'] = wetland_names * len(df)
+# Give the DataFrame a new name
+df['Name'] = wetland_name
 
 # Remove the old columns
-df = df.drop('Polygon Name', axis=1)
+df = df.drop('Filename', axis=1)
 df = df.drop('year', axis=1)
 df = df.drop('month', axis=1)
 
 # Rearrange the old columns
-df = df[['Name', 'Date', 'Area (square meters)']]
+df = df[['Name', 'Date', 'Area (metres squared)']]
 
 # Display the DataFrame
 print(df)
@@ -36,15 +36,15 @@ df['Date'] = pd.to_datetime(df['Date'], format='%m/%Y')
 
 # Create the scatter plot
 plt.figure(figsize=(8, 6))
-plt.scatter(df['Date'], df['Area (square meters)'], s=70)
+plt.scatter(df['Date'], df['Area (metres squared)'], s=70)
 plt.xlabel('Date')
 plt.ylabel('Area (m^2)')
-plt.title('Wetland Area from NDWI (2020-2023)')
+plt.title(f'{wetland_name} Area from NDWI (2020-2023)')
 
 # Disable scientific notation for the y-axis
 #plt.gca().get_yaxis().get_major_formatter().set_scientific(False)
 
-plt.savefig(f'{wetland_names}.png')
+plt.savefig(r'C:\Users\abro3569\PycharmProjects\gee\ndwi_area\\' + wetland_name + '.png')
 
 plt.show()
 
